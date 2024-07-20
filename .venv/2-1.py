6128698e23530968b505e52ac12ee6955cd9cfc2
flying_bucket <2311067@mail.nankai.edu.cn>
2024-07-20 17:50:47 +0800

    regular commit

--- .venv/2-1.py
+++ .venv/2-1.py
@@ -1,11 +1,44 @@
+'''第一部分'''
 import numpy as np
 arr2=np.array([list(range(i,i+3)) for i in [2,4,6]])
 list2=[list(range(i,i+3)) for i in [2,4,6]]
 list3=[list(range(i,i+3)) for i in [2,4,6]]
 arr3=np.array(list3)
-# print(arr2)
+print(arr2)
 print(list3)
-# print(arr3)
+print(arr3)
 
-# a=list(range(2,5))
-# print(a)
+a=list(range(2,5))
+print(a)
+
+
+'''第二部分'''
+import numpy as np
+arr1 = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([5, 4, 3, 2, 1])
+
+# 数组加法
+arr3 = arr1 + arr2
+
+# 数组减法
+arr4 = arr1 - arr2
+
+# 数组乘法
+arr5 = arr1 * arr2
+
+# 数组除法
+arr6 = arr1 / arr2
+
+print(arr3)
+print(arr4)
+print(arr5)
+print(arr6)
+
+'''第三部分'''
+import numpy as np
+arr7 = np.array([[1, 2], [3, 4]])
+arr8 = np.array([[5, 6], [7, 8]])
+
+# 矩阵乘法
+arr9 = arr7 @ arr8
+print(arr9)
